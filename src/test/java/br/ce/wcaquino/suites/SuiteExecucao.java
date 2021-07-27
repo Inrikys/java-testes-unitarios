7ab69697e8bb66b992aa497a9867ce0542bf4db3
Inrikys <inrikys@gmail.com>
2021-07-27 00:13:40 -0300

    Suite de teste

---
+++ src/test/java/br/ce/wcaquino/suites/SuiteExecucao.java
@@ -0,0 +1,20 @@
+package br.ce.wcaquino.suites;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+import br.ce.wcaquino.servicos.CalculadoraTest;
+import br.ce.wcaquino.servicos.CalculoValorLocacaoTest;
+import br.ce.wcaquino.servicos.LocacaoServiceTest;
+
+// Funciona para selecionar classes que serão executadas
+@RunWith(Suite.class)
+@SuiteClasses({
+	CalculadoraTest.class,
+	CalculoValorLocacaoTest.class,
+	LocacaoServiceTest.class
+})
+public class SuiteExecucao {
+	
+}
