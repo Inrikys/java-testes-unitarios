4a436864a87f13bfd35d7835ede5ddda0450c23e
Inrikys <inrikys@gmail.com>
2021-08-02 23:42:41 -0300

    Exemplo de Matchers do Mockito

--- src/test/java/br/ce/wcaquino/servicos/CalculadoraMockTest.java
+++ src/test/java/br/ce/wcaquino/servicos/CalculadoraMockTest.java
@@ -9,7 +9,7 @@ public class CalculadoraMockTest {
 	@Test
 	public void teste() {
 		Calculadora calc = Mockito.mock(Calculadora.class);
-		Mockito.when(calc.somar(1, 2)).thenReturn(5);
+		Mockito.when(calc.somar(Mockito.eq(1), Mockito.anyInt())).thenReturn(5);
 
 		System.out.println(calc.somar(1, 2));
 	}
